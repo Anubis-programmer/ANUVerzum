f4dcd2d435fa23b39993949b548d6e4d09d33ad8
Anubis-programmer <Anubis-programmer@users.noreply.github.com>
2026-05-18 14:27:32 +0200

    TS recommendation changed to ES2018, Features defaultComponent bugfix. (#35)

--- src/core/components/Feature.ts
+++ src/core/components/Feature.ts
@@ -1,5 +1,6 @@
 import { createElement, AnuElement, Props } from '../elements';
 import { createContext } from './Context';
+import { Fragment } from './Fragment';
 
 type FeaturesMap = Record<string, boolean>;
 
@@ -13,15 +14,23 @@ export interface FeatureProviderProps extends Props {
 
 export interface FeatureToggleProps extends Props {
     name: string;
-    defaultComponent?: AnuElement | null;
+    defaultComponent?: AnuElement | AnuElement[] | null;
 }
+
+const resolveDefault = (defaultComponent: AnuElement | AnuElement[] | null): AnuElement | AnuElement[] | null => {
+    if (defaultComponent && !Array.isArray(defaultComponent) && (defaultComponent as AnuElement).type === Fragment) {
+        return ((defaultComponent as AnuElement).props.children as AnuElement[]) ?? null;
+    }
+    
+    return defaultComponent;
+};
 
 const FeatureToggle = ({ name, children, defaultComponent = null }: FeatureToggleProps): AnuElement =>
     createElement(
         FeaturesContext.ContextConsumer,
         {},
         ({ value: { features } }: { value: { features?: FeaturesMap } }) =>
-            features && features[name] === true ? children : defaultComponent
+            features && features[name] === true ? children : resolveDefault(defaultComponent)
     );
 
 const Feature = {
